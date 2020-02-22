655d23f6c5a6eb2877fc143bb11cd240ab7ee72d
liroding <dingyinglai@126.com>
2020-02-22 16:25:44 +0800

    change model type null=True

--- wxapp/models.py
+++ wxapp/models.py
@@ -9,20 +9,20 @@ class login(models.Model):
 #    #authsession = models.CharField(max_length = 20)
     
 class usersmessagemysqldb(models.Model):
-    openid = models.CharField(max_length = 60)
-    loginstatus = models.CharField(max_length = 20)
-    authsession = models.CharField(max_length = 60)
+    openid = models.CharField(max_length = 60,null = True)
+    loginstatus = models.CharField(max_length = 20,null = True)
+    authsession = models.CharField(max_length = 60,null =True)
     nickname = models.CharField(max_length = 20,default='testname')
     
     #idusermessge
 
-    name = models.CharField(max_length = 20,default='')
-    sex = models.CharField(max_length = 20,default='')
-    age = models.CharField(max_length = 20,default='')
-    department = models.CharField(max_length = 20,default='')
-    telephone = models.CharField(max_length = 20,default='')
+    name = models.CharField(max_length = 20,null = True)
+    sex = models.CharField(max_length = 20,null = True)
+    age = models.CharField(max_length = 20,null = True)
+    department = models.CharField(max_length = 20,null = True)
+    telephone = models.CharField(max_length = 20,null = True)
    
     #patient case
-    caseimg = models.CharField(max_length = 50,default='')
+    caseimg = models.CharField(max_length = 50,null = True)
     # def __str__(self):
     #    return 'username:'+ self.username
